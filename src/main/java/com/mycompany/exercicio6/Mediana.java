7e1d1bca0478be3e2ea4742207c9aa04ec630551
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-08-29 12:55:51 -0300

    Feito a media menor valor e numeros abaixo e acima da media

--- src/main/java/com/mycompany/exercicio6/Mediana.java
+++ src/main/java/com/mycompany/exercicio6/Mediana.java
@@ -1,15 +1,55 @@
 package com.mycompany.exercicio6;
 
+import java.util.Arrays;
+
 public class Mediana {
-   
-    public double getMediana(double[] numeros) throws Exception{
+
+    public double getMediana(double[] numeros) throws Exception {
+        Arrays.sort(numeros);
         if (numeros == null || numeros.length <= 0) {
             throw new Exception();
+        } else if (numeros.length % 2 == 0) {
+            return (numeros[numeros.length / 2] + numeros[numeros.length / 2 - 1]) / 2;
+        } else if (numeros.length % 2 != 0) {
+            return numeros[numeros.length / 2];
         }
-        else if (numeros.length % 2 != 0) {
-            return numeros[numeros.length/2];
-        }
-        
+
         return 0;
+    }
+
+    public double getMedia(double[] numeros) {
+        return Arrays.stream(numeros).sum() / numeros.length;
+    }
+
+    public double getMenorValor(double[] numeros) {
+        Arrays.sort(numeros);
+        return numeros[0];
+    }
+
+    public double getMaiorValor(double[] numeros) {
+        Arrays.sort(numeros);
+        return numeros[numeros.length -1];
+    }
+
+    public double getNumeroAcimaMedia(double[] numeros) {
+        double media = getMedia(numeros);
+        double acima = 0.0f;
+        for (int i = 0; i < numeros.length; i++) {
+            if (media < numeros[i]) {
+                acima++;
+            }
+        }
+        return acima;
+    }
+
+    public double getNumeroAbaixoMedia(double[] numeros) {
+        double media = getMedia(numeros);
+        double abaixo = 0.0f;
+        for (int i = 0; i < numeros.length; i++) {
+            if (media > numeros[i]) {
+                abaixo++;
+            }
+        }
+        return abaixo;
     }
 }
--- src/test/java/MediaTest.java
+++ src/test/java/MediaTest.java
@@ -9,21 +9,67 @@ import static org.junit.Assert.*;
 
 public class MediaTest {
 
-    public MediaTest() {
-    }
+    Mediana m = new Mediana();
 
     @Test
     public void testeMedianaVetor() throws Exception {
         double[] numeros = {1, 2, 3, 4, 5};
-        Mediana m = new Mediana();
         double res = m.getMediana(numeros);
         assertEquals(3, res, 0.1);
     }
-    @Test (expected = Exception.class)
+
+    @Test
+    public void testeMedianaDesordenado() throws Exception {
+        double[] numeros = {5, 3, 2, 4, 1};
+        double res = m.getMediana(numeros);
+        assertEquals(3, res, 0.1);
+    }
+
+    @Test
+    public void testeMedianaPar() throws Exception {
+        double[] numeros = {1, 4, 6, 2, 7, 3};
+        double res = m.getMediana(numeros);
+        assertEquals(3.5, res, 0.1);
+    }
+
+    @Test
+    public void testeMedia() throws Exception {
+        double[] numeros = {1};
+        double res = m.getMedia(numeros);
+        assertEquals(1, res, 0.1);
+    }
+
+    @Test
+    public void testeMenorValor() throws Exception {
+        double[] numeros = {1, 5, 3, 4, 6};
+        double res = m.getMenorValor(numeros);
+        assertEquals(1, res, 0.1);
+    }
+
+    @Test
+    public void testeMaiorValor() throws Exception {
+        double[] numeros = {1, 5, 3, 4, 6};
+        double res = m.getMaiorValor(numeros);
+        assertEquals(6, res, 0.1);
+    }
+
+    @Test
+    public void testeNumeroAcimaMedia() throws Exception {
+        double[] numeros = {1, 4, 3, 6, 5};
+        double res = m.getNumeroAcimaMedia(numeros);
+        assertEquals(3, res, 0.1);
+    }
+    
+    @Test
+    public void testeNumeroAbaixoMedia() throws Exception {
+        double[] numeros = {1, 4, 3, 6, 5};
+        double res = m.getNumeroAbaixoMedia(numeros);
+        assertEquals(2, res, 0.1);
+    }
+    
+    @Test(expected = Exception.class)
     public void testeNulo() throws Exception {
         double[] numeros = null;
-        Mediana m = new Mediana();
         double res = m.getMediana(numeros);
-        
     }
 }
